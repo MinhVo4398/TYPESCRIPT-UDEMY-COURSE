bf95f21fd2af65555d37d944d70bb02e9a8ed0a3
MinhVo <minhvo4398@gmail.com>
2024-02-08 14:19:46 +0700

    variables

---
+++ variable.ts
@@ -0,0 +1,9 @@
+var myName = "Vignesh";
+let myAge = 25;
+const myGender = "Male";
+
+console.log("My name is " + myName + " And my age is " + myAge + " Am I am a "+ myGender);
+
+myName = "Playwright";
+myAge = 10;
+console.log("My name is " + myName + " And my age is " + myAge + " Am I am a "+ myGender);
