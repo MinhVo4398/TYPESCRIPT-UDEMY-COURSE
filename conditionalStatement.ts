4415ff4828a76fa57b94cea11377f0be7ace9f20
MinhVo <minhvo4398@gmail.com>
2024-02-09 11:58:48 +0700

    switch case

--- conditionalStatement.ts
+++ conditionalStatement.ts
@@ -1,24 +1,24 @@
 const myAgeNow = 15;
 
-if(myAgeNow >=18) {
+if (myAgeNow >= 18) {
     console.log("You are an adult");
 }
 
 // Else if statement
 let number = 10;
-if(number >0) {
+if (number > 0) {
     console.log('This is positive number');
 }
-else if(number <0) {
+else if (number < 0) {
     console.log("This is Negative number");
 }
 
 // The Else Statement
 let number1 = -10;
-if(number1 >0) {
+if (number1 > 0) {
     console.log('This is positive number');
 }
-else if(number1 <0) {
+else if (number1 < 0) {
     console.log("This is Negative number");
 }
 else {
@@ -27,14 +27,15 @@ else {
 
 // Nested Condition
 let num5 = 10;
-if(num5 >=0) {
-    if(num5 ===0){
+if (num5 >= 0) {
+    if (num5 === 0) {
         console.log("Number is zero");
     }
     else {
-        console.log("Number is Postive");
+        console.log("Number is Positive");
     }
 }
 else {
     console.log("Number is negative");
 }
+console.log("Test");
---
+++ switchStatement.ts
@@ -0,0 +1,22 @@
+let day = 3;
+let dayName;
+
+switch (day) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday"
+        break;
+    case 4:
+        dayName = "Thursday"
+        break;
+    case 5:
+        dayName = "Friday";
+    default:
+        dayName = "Weekend";
+}
+console.log(`It's ${dayName}`);
