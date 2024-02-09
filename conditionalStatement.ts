9b1bcc6d15de637708ef67a6496eedef0adbb92f
MinhVo <minhvo4398@gmail.com>
2024-02-09 09:15:30 +0700

    conditional statement

---
+++ conditionalStatement.ts
@@ -0,0 +1,40 @@
+const myAgeNow = 15;
+
+if(myAgeNow >=18) {
+    console.log("You are an adult");
+}
+
+// Else if statement
+let number = 10;
+if(number >0) {
+    console.log('This is positive number');
+}
+else if(number <0) {
+    console.log("This is Negative number");
+}
+
+// The Else Statement
+let number1 = -10;
+if(number1 >0) {
+    console.log('This is positive number');
+}
+else if(number1 <0) {
+    console.log("This is Negative number");
+}
+else {
+    console.log("This is a Zero");
+}
+
+// Nested Condition
+let num5 = 10;
+if(num5 >=0) {
+    if(num5 ===0){
+        console.log("Number is zero");
+    }
+    else {
+        console.log("Number is Postive");
+    }
+}
+else {
+    console.log("Number is negative");
+}
